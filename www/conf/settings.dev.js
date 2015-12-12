87e18e9c27baa0c9657270f233682eefbb1d81ea
chengan <453020675@qq.com>
2015-12-12 14:35:49 +0800

    图片压缩

--- www/conf/settings.dev.js
+++ www/conf/settings.dev.js
@@ -1,6 +1,6 @@
 AppSettings = {
-  baseApiUrl: 'dev.wdlst.lvtuan-pc-new',
-  /* baseApiUrl: '192.168.1.43', */
+  /* baseApiUrl: 'dev.wdlst.lvtuan-pc-new', */
+  baseApiUrl: '192.168.1.43',
   appId: 'wxf2c36d3bee50e935',
   secret: 'eae0369b72bcb49e18134dc447df475e'
 }
