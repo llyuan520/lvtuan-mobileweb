eb45d5a8b4651694530d518dbcf3b5b44a42d22b
chengan <453020675@qq.com>
2016-01-06 21:07:49 +0800

    样式、图标修改

--- www/js/module/auth.js
+++ www/js/module/auth.js
@@ -82,7 +82,6 @@ function userService($http, HOST, authService) {
 			// window.location.reload();
     	}
     ).catch(function(response) {
-		console.error('Gists error', response.status, response.data);
 		if (response.status === 400) {
 			var errMsg = JSON.stringify(data.error_messages.username);
 			layer.show(errMsg);
