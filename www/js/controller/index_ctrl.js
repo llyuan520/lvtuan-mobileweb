eb8bca91bf9b87f049a040457e2a424eb056ea7e
HYang <d2263516@gmail.com>
2016-01-21 20:18:46 +0800

    增加判断

--- www/js/controller/index_ctrl.js
+++ www/js/controller/index_ctrl.js
@@ -2252,6 +2252,17 @@ lvtuanApp.controller("viewCtrl",function($scope,$http,$rootScope,$stateParams,ht
    	sessionStorage.setItem("lawyerId", id);
    	sessionStorage.setItem("index", index);
 
+
+	
+	if (index == 5 && $scope.items.text_reply_fee == 0) {
+		layer.show("对不起，这个律师还没有开始提供这个服务!");
+		return;
+	}
+	if (index == 6 && $scope.items.phone_reply_fee == 0) {
+		layer.show("对不起，这个律师还没有开始提供这个服务!");
+		return;
+	}
+
    	var currentUser = authService.getUser();
 	if(currentUser.status == 1 || currentUser.status == 2){
 		//普通用户个人信息
